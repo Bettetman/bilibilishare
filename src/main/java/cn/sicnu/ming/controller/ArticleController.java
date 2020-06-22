6232b1585bc984bfbf4e48e9bb9618b33a144604
frank ming <frank.ming@foxmail.com>
2020-06-22 23:43:20 +0800

    首页展示查询

--- src/main/java/cn/sicnu/ming/controller/ArticleController.java
+++ src/main/java/cn/sicnu/ming/controller/ArticleController.java
@@ -7,7 +7,9 @@ import cn.sicnu.ming.util.HtmlUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -15,11 +17,12 @@ import java.util.Map;
 
 /**
  * @author frank ming
- * @createTime 2020617 12:22
- * @description 首页信息
+ * @createTime 20200622 19:02
+ * @description 首页的文章控制
  */
-@Controller
-public class IndexController {
+@RestController
+@RequestMapping(value = "/article")
+public class ArticleController {
 
     @Autowired
     private ArcTypeService arcTypeService;
@@ -28,22 +31,23 @@ public class IndexController {
     private ArticleService articleService;
 
     /**
-     * 首页
+     * 分页查询资源
+     * @param type
+     * @param currentPage
+     * @return
      */
-    @RequestMapping(value = "/")
-    public ModelAndView index(){
+    @RequestMapping("/{type}/{currentPage}")
+    public ModelAndView index(@PathVariable(value = "type",required = false) String type, @PathVariable(value = "currentPage",required = false) Integer currentPage){
         ModelAndView mav = new ModelAndView();
         mav.setViewName("index");
         //类型的html代码
         List arcTypeList = arcTypeService.listAll(Sort.Direction.ASC,"sort");
-        mav.addObject("arcTypeStr", HtmlUtil.getArcTypeStr("all",arcTypeList));
+        mav.addObject("arcTypeStr", HtmlUtil.getArcTypeStr(type,arcTypeList));
         //资源列表
-        Map<String, Object> map = articleService.list("all",1, ConstUtil.PAGE_SIZE);
+        Map<String, Object> map = articleService.list(type,currentPage, ConstUtil.PAGE_SIZE);
         mav.addObject("articleList",map.get("data"));
         //分页代码
-        mav.addObject("pageStr",HtmlUtil.getPagation("/article/all",Integer.parseInt(String.valueOf(map.get("count"))),1,"该分类还没有数据..."));
+        mav.addObject("pageStr",HtmlUtil.getPagation("/article/"+type,Integer.parseInt(String.valueOf(map.get("count"))),currentPage,"该分类还没有数据..."));
         return mav;
     }
-
-
 }
--- src/main/java/cn/sicnu/ming/util/HtmlUtil.java
+++ src/main/java/cn/sicnu/ming/util/HtmlUtil.java
@@ -32,4 +32,45 @@ public class HtmlUtil {
         }
         return arcTypeCode.toString();
     }
+
+    /**
+     * 拼接分页代码
+     * @param targetUrl 请求路径
+     * @param count     总记录数
+     * @param currentPage   当前页
+     * @param msg           没查到数据的时候的显示内容
+     * @return
+     */
+    public static String getPagation(String targetUrl,int count,int currentPage, String msg){
+        //总页数
+        int totalPage = count%ConstUtil.PAGE_SIZE == 0?count/ConstUtil.PAGE_SIZE:(count/ConstUtil.PAGE_SIZE)+1;
+        StringBuffer pageCode = new StringBuffer();
+        pageCode.append("<div class=\"laypage-main\">");
+        if(totalPage>0){
+            pageCode.append("<a href=\""+targetUrl+"/1\">首页</a>");
+        }
+        if(currentPage!=1){
+            pageCode.append("<a style=\"display: inline-block;\" href=\""+targetUrl+"/" + (currentPage - 1) + "\">上一页</a>");
+        }
+        for(int i = currentPage - 3;i <=currentPage +3;i++){
+            if(i<1||i>totalPage){
+                continue;
+            }
+            if(i == currentPage){
+                pageCode.append("<span class=\"laypage-curr\">" + i +"</span>");
+            }else{
+                pageCode.append("<a href=\""+targetUrl+"/"+i+"\">"+i+"</a>");
+            }
+        }
+        if(currentPage<totalPage){
+            pageCode.append("<a style=\"display: inline-block;\" href=\""+targetUrl+"/" + (currentPage + 1) + "\">下一页</a>");
+        }
+        if(totalPage>0){
+            pageCode.append("<a href=\""+targetUrl+"/"+totalPage+"\">尾页</a>");
+        }else{
+            pageCode.append("<span>"+msg+"</span>");
+        }
+        pageCode.append("</div>");
+        return pageCode.toString();
+    }
 }
